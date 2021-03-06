5a2c94326108d7e44b8c69e0f43d63b67553da30
JerJohnston <59575963+JerJohnston@users.noreply.github.com>
2021-03-06 01:34:20 -0700

    implementing JavaScript

--- js/main.js
+++ js/main.js
@@ -18,6 +18,75 @@ window.addEventListener('load', function(e){
       }
   })
 
+  // Project Page Templates
+
+  // Reference Variables
+
+  let projectArray = [];
+  let projectToggle = document.querySelector('.project-toggle');
+
+  const projects = fetch("/js/json/projects.json")
+    .then((response) => response.json());
+
+  
+
+  projects.then((data) => {
+
+    projectArray = [...data];
+
+  })
+
+  // adding the project list to the DOM
+
+  projectToggle.addEventListener('click', (e) => {
+
+    const projectListContent = document.querySelector('.project-list');
+  
+    const projectListTemplate = `
+      <div class="project-list>
+           <ul class="projects">
+
+           </ul>
+        </div>
+    `
+
+    const displayProjectList = document
+      .createRange()
+      .createContextualFragment(projectListTemplate);
+
+    projectListContent.appendChild(displayProjectList);
+
   
   
+    projectArray.map(function (value, index) {
+       const id = value.id;
+       const name = value.name;
+        const projectMenu = document.querySelector('.project-list')
+      
+        const projectListTemplate = `
+        <ul class="project-list">
+           <li class="project-item">
+                <a href="#" class="project-link" data-id="${id}">${name}</a>
+            </li>
+         </ul>
+       `
+
+        const projectList = document
+           .createRange()
+           .createContextualFragment(projectListTemplate)
+           .querySelector('.project-item');
+
+         projectMenu.appendChild(projectList);
+      
+     })
+
+     document.querySelectorAll('.project-link')
+                .forEach(project => {
+                    project.addEventListener('click', (e) => {
+
+                      
+                    })
+                })
+  })
+
 })
